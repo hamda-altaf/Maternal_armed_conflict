6906b7c21002e8da03abc060c7c0523c4aeafaac
hamda-altaf <humda6980@gmail.com>
2024-10-07 13:00:54 -0400

    Class 5 update

--- scripts/analytical_data.R
+++ scripts/analytical_data.R
@@ -1,6 +1,8 @@
 library(here)
 library(dplyr)
 here()
+install.packages("purrr")
+library(purrr)
 covs <- read.csv(here('Original', 'covariates.csv')) 
 #use source to call the R scripts that create the individual datasets
 source(here('scripts', 'create_disaster.R')) 
@@ -19,7 +21,8 @@ analytical_data <- left_join(covs, final_merged_data, by = c("ISO", "year"))
 analytical_data<- analytical_data %>%
   mutate(drought = replace_na(drought, 0),
          earthquake = replace_na(earthquake, 0),
-         totaldeath = replace_na(totaldeath, 0))
+         totaldeath = replace_na(totaldeath, 0),
+         conflict = replace_na(conflict, 0))
 
 write.csv(analytical_data, file = here("analytical_data", "analyticaldata.csv"), row.names = FALSE)
 
